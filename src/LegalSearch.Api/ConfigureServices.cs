23499f97e64878e56afc7be8f5eb2d8edb26eca3
Eseosa Ehioghae <eseosaehioghae@gmail.com>
2023-06-02 20:04:28 +0100

    refactor API services configuration

---
+++ src/LegalSearch.Api/ConfigureServices.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using LegalSearch.Application.Models.Constants;
+using LegalSearch.Infrastructure.Services.Auth;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace LegalSearch.Api
+{
+    public static class ConfigureServices
+    {
+        /// <summary>
+        /// Add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        public static void AddServicesToContainer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.ConfigureAuthentication(configuration);
+            services.AddRouting(x => x.LowercaseUrls = true);
+            services.AddHttpContextAccessor();
+            services.AddHttpClient();
+            services.AddHealthChecks();
+            services.AddDistributedMemoryCache();
+            services.AddControllers();
+
+            services.Configure<ApiBehaviorOptions>(options => { options.SuppressModelStateInvalidFilter = true; });
+            
+            // services.AddDbContext<AppDbContext>(x =>
+            // {
+            //     x.UseNpgsql(AppConstants.DbConnectionString, o => o.MigrationsAssembly("LegalSearch.Infrastructure"));
+            // });
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.ConfigureSwagger();
+        }
+
+        /// <summary>
+        /// Configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        public static void ConfigureHttpRequestPipeline(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Autopost.Api v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseHttpsRedirection();
+            
+            // app.UseGlobalExceptionHandler();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllers();
+        }
+        
+        /// <summary>
+        /// Configures the JWT Authentication.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        private static void ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwt(configuration);
+        }
+
+        private static void AddJwt(this AuthenticationBuilder builder, IConfiguration configuration)
+        {
+            var keyParam = configuration["JWTConfig:Key"]!;
+            var key = Encoding.ASCII.GetBytes(keyParam);
+            builder.AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = JwtTokenGenerator.ApiIssuer,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    RequireExpirationTime = false
+                };
+            });
+        }
+        
+        private static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Legal Search API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "SBSC",
+                        Email = string.Empty,
+                        Url = new Uri("https://www.sbsc.com/"),
+                    }
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+    }
+}
--- src/LegalSearch.Api/Program.cs
+++ src/LegalSearch.Api/Program.cs
@@ -1,20 +1,14 @@
+using LegalSearch.Api;
+using LegalSearch.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var configuration = builder.Configuration;
+
+builder.Services.AddServicesToContainer(configuration);
+builder.Services.ConfigureInfrastructureServices(configuration);
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
+app.ConfigureHttpRequestPipeline();
 app.Run();
