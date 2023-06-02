43f7f05182b9be4f08744b1a3a31930cde8c4547
Eseosa Ehioghae <eseosaehioghae@gmail.com>
2023-06-02 20:35:29 +0100

    add BaseEntity

---
+++ src/LegalSearch.Domain/Common/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LegalSearch.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        protected BaseEntity()
+        {
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+        
+        [Key]
+        public Guid Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
