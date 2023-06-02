4ccb934dcc82e43174ad726de32cf12f6ce83a76
Eseosa Ehioghae <eseosaehioghae@gmail.com>
2023-06-02 20:31:51 +0100

    add Role entity and configuration

---
+++ src/LegalSearch.Domain/Entities/Role/Role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace LegalSearch.Domain.Entities.Role
+{
+    public class Role : IdentityRole<Guid>
+    {
+        public Role()
+        {
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+        
+        public List<string> Permissions { get; set; } = new();
+        
+        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
---
+++ src/LegalSearch.Infrastructure/Persistence/Configurations/RoleConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using LegalSearch.Domain.Entities.Role;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LegalSearch.Infrastructure.Persistence.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.HasQueryFilter(x => !x.IsDeleted);
+            
+            builder.HasIndex(c => c.Name)
+                .IsUnique()
+                .HasFilter($" \"{nameof(Role.IsDeleted)}\" = false");
+        }
+    }
+}
