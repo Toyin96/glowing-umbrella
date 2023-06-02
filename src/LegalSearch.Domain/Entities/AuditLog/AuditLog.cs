90fc353cff51fb47a309cf6cad4801013775046f
Eseosa Ehioghae <eseosaehioghae@gmail.com>
2023-06-02 20:32:54 +0100

    add AuditLog entity and configuration

---
+++ src/LegalSearch.Domain/Entities/AuditLog/AuditLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using LegalSearch.Domain.Common;
+
+namespace LegalSearch.Domain.Entities.AuditLog
+{
+    public class AuditLog : BaseEntity
+    {
+        public Guid? ActorId { get; set; }
+        
+        public Guid? EntityId { get; set; }
+        
+        public string EntityTable { get; set; }
+        
+        public AuditAction AuditAction { get; set; }
+        
+        public string Log { get; set; }
+
+        public bool IsSuccessful { get; set; } = true;
+    }
+    
+    public enum AuditAction
+    {
+        Auth
+    }
+}
---
+++ src/LegalSearch.Infrastructure/Persistence/Configurations/AuditLogConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using LegalSearch.Domain.Entities.AuditLog;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LegalSearch.Infrastructure.Persistence.Configurations
+{
+    public class AuditLogConfiguration : IEntityTypeConfiguration<AuditLog>
+    {
+        public void Configure(EntityTypeBuilder<AuditLog> builder)
+        {
+            builder.HasQueryFilter(x => !x.IsDeleted);
+        }
+    }
+}
