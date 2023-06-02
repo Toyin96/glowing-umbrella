fcc552a0cb0c46d3640848285c7bb2263116c585
Eseosa Ehioghae <eseosaehioghae@gmail.com>
2023-06-02 20:29:32 +0100

    add User entity and configuration

---
+++ src/LegalSearch.Domain/Entities/User/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace LegalSearch.Domain.Entities.User
+{
+    public class User : IdentityUser<Guid>
+    {
+        public User()
+        {
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
