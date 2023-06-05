120f559c4c539f26d5dd7dd28389053df682d1d4
Eseosa Ehioghae <eseosaehioghae@gmail.com>
2023-06-05 15:49:04 +0100

    implement Solicitor onbaording

---
+++ src/LegalSearch.Infrastructure/Services/User/Events/SolicitorCreatedEvent.cs
@@ -0,0 +1,7 @@
+﻿using LegalSearch.Domain.Entities.User.Solicitor;
+using MediatR;
+
+namespace LegalSearch.Infrastructure.Services.User.Events
+{
+    public record SolicitorCreatedEvent(Solicitor User, string DefaultPassword) : INotification;
+}
