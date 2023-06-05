9d9549887c4ac8c43ce81f614d8f0fce61b55fb3
Eseosa Ehioghae <eseosaehioghae@gmail.com>
2023-06-05 15:38:39 +0100

    create bank and bank account entities and configuration

---
+++ src/LegalSearch.Infrastructure/Persistence/Configurations/BankAccountConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using LegalSearch.Domain.Entities.User.Solicitor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LegalSearch.Infrastructure.Persistence.Configurations
+{
+    public class BankAccountConfiguration : IEntityTypeConfiguration<BankAccount>
+    {
+        public void Configure(EntityTypeBuilder<BankAccount> builder)
+        {
+            builder.HasQueryFilter(x => !x.IsDeleted);
+
+            builder.HasIndex(c => new{c.AccountNumber, c.BankId}, "Idx_OneUniqueAccountInBank")
+                .IsUnique()
+                .HasFilter($" \"{nameof(BankAccount.IsDeleted)}\" = false");
+        }
+    }
+    
+    public class BankConfiguration : IEntityTypeConfiguration<Bank>
+    {
+        public void Configure(EntityTypeBuilder<Bank> builder)
+        {
+            builder.HasQueryFilter(x => !x.IsDeleted);
+
+            builder.HasIndex(c => c.Name)
+                .IsUnique()
+                .HasFilter($" \"{nameof(Bank.IsDeleted)}\" = false");
+        }
+    }
+}
