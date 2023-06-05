e344ff0f17b08fdf75d26384ff0c46e2d833185d
Eseosa Ehioghae <eseosaehioghae@gmail.com>
2023-06-05 15:46:18 +0100

    create solicitor entity and configuration

---
+++ src/LegalSearch.Domain/Entities/User/Solicitor/Solicitor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LegalSearch.Domain.Entities.User.Solicitor
+{
+    public class Solicitor : User
+    {
+        public string Address { get; set; }
+        public Guid FirmId { get; set; }
+        public Firm Firm { get; set; }
+        public Guid StateId { get; set; }
+        public State State { get; set; }
+        public Guid LgaId { get; set; }
+        public Lga Lga { get; set; }
+        public Guid BankAccountId { get; set; }
+        public Guid BankAccount { get; set; }
+    }
+}
---
+++ src/LegalSearch.Infrastructure/Persistence/Configurations/SolicitorConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using LegalSearch.Domain.Entities.User.Solicitor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LegalSearch.Infrastructure.Persistence.Configurations
+{
+    public class SolicitorConfiguration : IEntityTypeConfiguration<Solicitor>
+    {
+        public void Configure(EntityTypeBuilder<Solicitor> builder)
+        {
+            builder.HasQueryFilter(x => !x.IsDeleted);
+            
+            builder.HasIndex(c => new { c.FirstName, c.LastName })
+                .HasFilter($" \"{nameof(Solicitor.IsDeleted)}\" = false");
+        }
+    }
+}
