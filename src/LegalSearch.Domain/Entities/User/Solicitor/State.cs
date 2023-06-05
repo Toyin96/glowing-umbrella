0f89a63196d5aa8b0ec2720ca134e965dd906a29
Eseosa Ehioghae <eseosaehioghae@gmail.com>
2023-06-05 15:30:33 +0100

    create state and lga entities and configuration

---
+++ src/LegalSearch.Domain/Entities/User/Solicitor/State.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using LegalSearch.Domain.Common;
+
+namespace LegalSearch.Domain.Entities.User.Solicitor
+{
+    public class State : BaseEntity
+    {
+        public string Name { get; set; }
+        public ICollection<Lga> Lgas { get; set; }
+    }
+    
+    public class Lga : BaseEntity
+    {
+        public string Name { get; set; }
+        public Guid StateId { get; set; }
+        public State State { get; set; }
+    }
+}
---
+++ src/LegalSearch.Infrastructure/Persistence/Configurations/StateConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using LegalSearch.Domain.Entities.User.Solicitor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LegalSearch.Infrastructure.Persistence.Configurations
+{
+    public class StateConfiguration : IEntityTypeConfiguration<State>
+    {
+        public void Configure(EntityTypeBuilder<State> builder)
+        {
+            builder.HasQueryFilter(x => !x.IsDeleted);
+            
+            builder.HasIndex(c => c.Name)
+                .IsUnique()
+                .HasFilter($" \"{nameof(State.IsDeleted)}\" = false");
+        }
+    }
+    public class LgaConfiguration : IEntityTypeConfiguration<Lga>
+    {
+        public void Configure(EntityTypeBuilder<Lga> builder)
+        {
+            builder.HasQueryFilter(x => !x.IsDeleted);
+            
+            builder.HasIndex(c => new {c.Name, c.StateId})
+                .IsUnique()
+                .HasFilter($" \"{nameof(Lga.IsDeleted)}\" = false");
+        }
+    }
+}
