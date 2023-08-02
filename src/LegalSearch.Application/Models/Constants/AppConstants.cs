225c568e492b8f833ac1d16ca30c174a11078523
Toyin96 <toyin.onagoruwa@wemabank.com>
2023-08-02 17:48:04 +0100

    removed local database connection string

--- src/LegalSearch.Application/Models/Constants/AppConstants.cs
+++ src/LegalSearch.Application/Models/Constants/AppConstants.cs
@@ -1,10 +1,8 @@
-﻿using System;
-
-namespace LegalSearch.Application.Models.Constants
+﻿namespace LegalSearch.Application.Models.Constants
 {
     public static class AppConstants
     {
         public static readonly string DbConnectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING") 
-            ?? "Data Source=WEMA-WDB-L9396;Initial Catalog=LegalSearch;Integrated Security=True";
+            ?? "Data Source=WEMA-WDB-L9396;Initial Catalog=LegalSearch;Trusted_Connection=True;TrustServerCertificate=True;Integrated Security=True;Encrypt=False;";
     }
 }
